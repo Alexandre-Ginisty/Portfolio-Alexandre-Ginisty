826be2231a076c421a9001bb4a80d81b352bee5a
Alexandre Ginisty <alexandre.ginisty@example.com>
2025-09-01 19:23:03 +0200

    hhh

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Commentez ces lignes en développement :
+  // En développement, on ne veut pas de 'output: export'
   // output: 'export',
   // trailingSlash: true,
   images: {
     unoptimized: true,
   },
 };
+
+module.exports = nextConfig;
